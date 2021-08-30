4854e4a05ebcf13bc42adc8be156f1c06d227ce8
Jerry D. Harthcock <SYMPL.gpu@gmail.com>
2021-08-30 10:14:19 -0500

    Add files via upload

---
+++ SYMPL_demo1.bat
@@ -0,0 +1,2 @@
+ customasm -f annotated -o SYMPL_demo1_BT_custom.lst -s SYMPL_demo1_BT_custom.sym SYMPL_demo1_BT_custom.asm
+ customasm -f binary -o SYMPL_demo1_BT_custom.hex -s SYMPL_demo1_BT_custom.sym SYMPL_demo1_BT_custom.asm
