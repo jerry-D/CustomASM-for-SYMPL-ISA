42c418c67b5bd94aa0dbfdc56098e421249189e4
Jerry D. Harthcock <SYMPL.gpu@gmail.com>
2023-07-19 16:33:20 -0500

    Update SYMPL_demo1.bat

--- SYMPL_demo1.bat
+++ SYMPL_demo1.bat
@@ -1,2 +1,11 @@
- customasm -f annotated -o SYMPL_demo1_BT_custom.lst -s SYMPL_demo1_BT_custom.sym SYMPL_demo1_BT_custom.asm
- customasm -f binary -o SYMPL_demo1_BT_custom.hex -s SYMPL_demo1_BT_custom.sym SYMPL_demo1_BT_custom.asm
+:: use the following command to output a object listing file
+customasm -f annotatedhex -o SYMPL_demo1_BT_custom.obj SYMPL_demo1_BT_custom.asm
+ 
+:: use the following command to output a executable binary file that can be uploaded to the target
+customasm -f binary -o SYMPL_demo1_BT_custom.bin SYMPL_demo1_BT_custom.asm
+
+:: use the following command to output a source-only listing file that can be uploaded to the debugger
+customasm -f annotated -o SYMPL_demo1_BT_custom.src SYMPL_demo1_BT_custom.asm
+ 
+:: use the following command to produce a 64-bit wide hex file that can be used by Verilog $readmemh system task for initializing FPGA ROM
+customasm -f hexdump -o SYMPL_demo1_BT_custom.ini SYMPL_demo1_BT_custom.asm
